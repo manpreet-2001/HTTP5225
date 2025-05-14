580d19e69a13919ad8bab29609cf81fa426d4dca
Manpreet <manpreet123singh987@gmail.com>
2025-05-14 16:38:41 -0400

    Add Week2 index.php

---
+++ Week2/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    <!-- Learnin Echo -->
+    <?php 
+    echo "<h1> Hello World! </h1>";
+    echo "<p> My Name is Manpreet Singh. </p>";
+    ?>
+
+    <hr>
+
+    <!-- Learning Variables -->
+    <?php 
+    $fname = "Manpreet";
+    $lname = "Singh";
+    
+    echo "<h1> Hello " . "$fname " . "$lname";
+    ?>
+
+
+</body>
+</html>
